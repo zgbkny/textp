bbe6abc1371aa5d74fc17da711c5abc9f4ba26ae
zgbkny <zgbkny@gmail.com>
2014-05-25 10:17:10 +0800

    first commit

---
+++ dip2p.py
@@ -0,0 +1,14 @@
+ipath = "biodata/ECOLI_MAP.txt";
+dpath = "biodata/DIP_20101010.txt";
+fi = open(ipath, "r")
+fd = open(ipath, "r")
+
+for line in fi:
+    print(line)
+
+
+
+fi.close()
+
+fd.close()
+
